bfd106a434366e6c90a8384880229b04fd15503d
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-03 16:42:57 +0300

    registration and login pages

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-
-document.querySelector("body").appendChild(h2);
+//this code validates that indeed the user has filled in all the fields
+//if not, the user is prompted to do so
+    function validateLoginForm(){
+        if(document.forms['input']['usrname'].value == ''){
+            alert('please provide a username!');
+            document.forms['input']['usrname'].focus();
+            return false
+        }
+        if(document.forms['input']['password'].value == ''){
+            alert('Please provide password');
+            document.forms['input']['password'].focus();
+            return false
+        }
+        if(document.forms['input']['usrname'].value != '' && document.forms['input']['password'].value != ''){
+            alert('You are logged in as ' + document.forms['input']['usrname'].value) 
+            return false
+        }
+        return true;
+        }
+//this function validates the user login form
+    function validateRegistrationForm(){
+        if(document.forms['signup']['firstname'].value == ''){
+            alert('please provide a firstname!');
+            document.forms['signup']['firstname'].focus();
+            return false;
+        }
+        if(document.forms['signup']['lastname'].value == ''){
+            alert('Please provide lastname');
+            document.forms['signup']['lastname'].focus();
+            return false;
+        }
+         if(document.forms['signup']['username'].value == ''){
+            alert('please provide a username!');
+            document.forms['signup']['username'].focus();
+            return false;
+        }
+        if(document.forms['signup']['email'].value == ''){
+            alert('Please provide email');
+            document.forms['signup']['email'].focus();
+            return false;
+        }
+        if(document.forms['signup']['password'].value == ''){
+            alert('please provide a password!');
+            document.forms['signup']['password'].focus();
+            return false;
+        }
+        alert('Congratulations!you have successfully created an account, navigate to the Login Page!')
+        return true;
+   }
